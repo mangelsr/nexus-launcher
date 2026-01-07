d304515b3f2ff6bb07ebb88cb5143150f8da2296
Miguel Sanchez <mangelsr25@gmail.com>
2026-01-07 10:38:26 -0500

    [UPD] Window size dimentions and initial state

--- src/main/windows/main.window.ts
+++ src/main/windows/main.window.ts
@@ -5,12 +5,13 @@ declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
 
 const createMainWindow = (): void => {
     const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1280,
+        height: 720,
         webPreferences: {
             preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
         },
     });
+    mainWindow.maximize();
     mainWindow.setMenu(null);
     mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
     // mainWindow.webContents.openDevTools();
